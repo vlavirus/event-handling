aad25cb39c72a62e59b522dc19cb8be43dff0c31
vlavirus <androando777@gmail.com>
2021-03-05 19:47:16 +0200

    feat(notification) add push notification

--- src/app/shared/components/notification/notification.component.ts
+++ src/app/shared/components/notification/notification.component.ts
@@ -11,17 +11,17 @@ export class NotificationComponent implements OnInit {
   readonly VAPID_PUBLIC_KEY = "BI0OKWncKRuNh1vB9d2ewGj_uNXyKhUBBBClkf9u1gYK2dJ280YdXf-J3X7eoY0eXfRKvzLDL8P1LhVnlVUOLy0";
 
   sub = {
-    "endpoint":"https://fcm.googleapis.com/fcm/send/fnYJmD92ZDc:APA91bHd_y8yCLH77z3mXcWWbo4F1TonqStyruec-ZmeDUx7wpO0nOKClvWyEN43xycpOuM7NMnHyVbOh3S4SXPYqHXF86DDnSCJS9tQpHNjcQKVGE81HUttNquLYiHrk2TqBEoPxGlB",
-    "expirationTime":null,
+    "endpoint": "https://fcm.googleapis.com/fcm/send/fnYJmD92ZDc:APA91bHd_y8yCLH77z3mXcWWbo4F1TonqStyruec-ZmeDUx7wpO0nOKClvWyEN43xycpOuM7NMnHyVbOh3S4SXPYqHXF86DDnSCJS9tQpHNjcQKVGE81HUttNquLYiHrk2TqBEoPxGlB",
+    "expirationTime": null,
     "keys": {
-      "p256dh":"BBX6F4osOhLq45imVW5HCaqepQ7w9rLVJB-fL27K1okcEQx3xmdyeaxGU0NNplxlobugZ23WonQUcTul2KFZSME",
-      "auth":"QzG4GGda4AzCeYbtLGZ8mQ"
+      "p256dh": "BBX6F4osOhLq45imVW5HCaqepQ7w9rLVJB-fL27K1okcEQx3xmdyeaxGU0NNplxlobugZ23WonQUcTul2KFZSME",
+      "auth": "QzG4GGda4AzCeYbtLGZ8mQ"
     }
   }
 
   payload = {
     "notification": {
-      "data": { url: 'https://www.google.com/'},
+      "data": {url: 'https://www.google.com/'},
       "title": 'Hello World',
       "vibrate": [100, 50, 100]
     }
@@ -30,7 +30,8 @@ export class NotificationComponent implements OnInit {
   constructor(
     private swPush: SwPush,
     // private newsletterService: NewsletterService
-  ) { }
+  ) {
+  }
 
   ngOnInit(): void {
   }
@@ -50,7 +51,5 @@ export class NotificationComponent implements OnInit {
     if (!this.swPush.isEnabled) {
       console.log("Notification is not enable");
     }
-
-
-
+  }
 }
